b90afa7353c77d0b866d53a2f91b110ee39b3a8c
Martikainen <oliver.martikainen@cgi.com>
2019-09-20 17:03:02 +0300

    Update databaseToLocal.js

--- formats/databaseToLocal.js
+++ formats/databaseToLocal.js
@@ -2,12 +2,13 @@
 //could add reports to this, but then i'd be doing this every update not every 1h
 //or could update this every 1h but update reports part every 6s to avoid making these calculations
 const setTeams = (teams_db, agents_db, profiles_db) => {
+    console.log('what1')
     if (agents_db.length != profiles_db.length) {
         console.log('Agents count mismatch', agents_db.length, profiles_db.length)
     }
 
     //frontend expects this structure - if changed OptionsSection needs change
-    const AgentProfile = (profile) => {
+    const AgentProfiler = (profile) => {
         const agent = agents_db.find(agent => agent.AgentId === profile.AgentId) //connects profile with agent & Team
         return ({
             'AgentName': `${agent.LastName} ${agent.FirstName}`, //name used in options
@@ -16,7 +17,7 @@ const setTeams = (teams_db, agents_db, profiles_db) => {
             'ServiceIds': profile.Profiles.map(list => list.ServiceId) //list used to filter queue
         })
     }
-    const TeamProfile = (team, AgentProfiles) => {
+    const TeamProfiler = (team, AgentProfiles) => {
         const teamProfiles = AgentProfiles.filter(p => p.TeamName === team.TeamName)
         return ({
             'TeamName': team.TeamName,
@@ -29,39 +30,36 @@ const setTeams = (teams_db, agents_db, profiles_db) => {
         return allServices.filter((item, index) => allServices.indexOf(item) === index) //removes duplicates - indexOf returns first item, if index is different == duplicate
     }
 
-    //const TeamSorter
+    //const TeamSorter - could do sorting in backend atm done in frontend
     //const ProfileSorter 
 
     /*ABOVE HELPER FUNCTIONS - BELOW DATA TRANSFORMATION PROCESS*/
 
-    const AgentProfiles = profiles_db.map(profile => AgentProfile(profile))
-    const TeamProfiles = teams_db.map(team => TeamProfile(team, AgentProfiles))
-    const allTeams = 'ALL TEAMS' //name of TEAM with all profiles
-    TeamProfiles.unshift({ 'TeamName': allTeams, 'Profiles': AgentProfiles })
+    const AgentProfiles = profiles_db.map(profile => AgentProfiler(profile))
+    const TeamProfiles = teams_db.map(team => TeamProfiler(team, AgentProfiles))
+    const allTeamsProfile = {
+        'AgentName': 'ALL TEAMS', //name used in options
+        'AgentId': 0,
+        'TeamName': 'ALL TEAMS',
+        'ServiceIds': ALL_TEAM_services(AgentProfiles) //list used to filter queue
+    }
+    const allTeamsTeam = {
+        'TeamName': 'ALL TEAMS',
+        'Profiles': [allTeamsProfile]
+    }
 
     //Add an ALL TEAMNAME PROFILES for each team
     TeamProfiles.forEach((team, index) => {
         ALL_TEAM = {
-            'AgentName': (team.TeamName !== allTeams) ? (`ALL ${team.TeamName}`) : allTeams, //frontend StatsCounter & OptionsSection (eg Profile sorting) depend on this format - changing this requires changes there
-            'AgentId': index, //Could cause issues if database has same agentid - agentid seem to start from 20000 though
+            'AgentName': `ALL ${team.TeamName}`, //frontend StatsCounter & OptionsSection (eg Profile sorting) depend on this format - changing this requires changes there
+            'AgentId': index+1, //Could cause issues if database has same agentid - agentid seem to start from 20000 though
             'TeamName': team.TeamName,
             'ServiceIds': ALL_TEAM_services(team.Profiles) //all teams services in one profile
         }
         team.Profiles.unshift(ALL_TEAM)
     })
-    //Add all ALL TEAMNAME profiles to ALL TEAMS team
-    const allTeamsProfile = TeamProfiles.find(t => t.TeamName === allTeams)
-    TeamProfiles.forEach(t => {
-        if (t.TeamName !== allTeams) {
-            const tAll = t.Profiles.find(p => p.AgentName === `ALL ${t.TeamName}`)
-            allTeamsProfile.Profiles.unshift({
-                'AgentName': tAll.AgentName,
-                'AgentId': tAll.AgentId,
-                'TeamName': t.TeamName, //needed to give correct teamname
-                'ServiceIds': tAll.ServiceIds,
-            })
-        }
-    })
+    TeamProfiles.unshift(allTeamsTeam)
+
     return TeamProfiles
 }
 
