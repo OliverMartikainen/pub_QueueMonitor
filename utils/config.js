3e69c27f649aa9e070b5ae776176cd2077f962ef
Martikainen <oliver.martikainen@cgi.com>
2019-09-30 17:18:27 +0300

    temp version solution

--- utils/config.js
+++ utils/config.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT
 const REST_URI = process.env.REST_URI
 const API_KEY = process.env.API_KEY
 const OC_NAME = process.env.OC_NAME
-const SERVER_VERSION = process.env.npm_package_version
-
+//const SERVER_VERSION = process.env.npm_package_version // - package.json version - atm requires complete server restart
+const SERVER_VERSION = '0.0.2' //this way npm run watch - forces frontend to update
 
 module.exports = {
     PORT,
@@ -14,22 +14,3 @@ module.exports = {
     OC_NAME,
     SERVER_VERSION
 }
-
-/*
-express https://fullstackopen.com/en/part3/node_js_and_express
-
-nodemon (same) - better dev workflow (browser auto reload)
-
-
-cors - middleware for cross origin resource sharing
-dotenv - .env environmental variable file handling DONE
-eslint - style/uniform code
-
-PART 4
-jest - testing library
-cross-env - different env modes (eg test, dev, prod)
-supertest - more test 
-
-bcrypt - good pw hasher
-jsonwebtoken - token part once user/login implementation
-*/
