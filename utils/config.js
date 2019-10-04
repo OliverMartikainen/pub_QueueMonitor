256af78657684ebd14439a05708d2735686e39ab
Martikainen <oliver.martikainen@cgi.com>
2019-10-04 15:26:54 +0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,7 @@ const REST_URI = process.env.REST_URI
 const API_KEY = process.env.API_KEY
 const OC_NAME = process.env.OC_NAME
 //const SERVER_VERSION = process.env.npm_package_version // - package.json version - atm requires complete server restart
-const SERVER_VERSION = '0.0.3' //this way npm run watch - forces frontend to update
+const SERVER_VERSION = '0.0.5' //this way npm run watch - forces frontend to update
 
 module.exports = {
     PORT,
