a5aa6fbe0861e593e9f0f1763a3283cffeada8b2
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2020-01-26 16:35:26 +0200

    update build to 0.1.5 version

---
+++ build/precache-manifest.3ae3a1227abcfaceab39ac4df1920a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788b6a8da3bfc5d7a00971cded2a9b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466e938b54039ed6a873",
+    "url": "/static/css/main.271d3bfd.chunk.css"
+  },
+  {
+    "revision": "7b0fa6a13a6eb4c309b9",
+    "url": "/static/js/2.ad383e72.chunk.js"
+  },
+  {
+    "revision": "466e938b54039ed6a873",
+    "url": "/static/js/main.07ab23d8.chunk.js"
+  },
+  {
+    "revision": "4b54749b57a191703f56",
+    "url": "/static/js/runtime~main.e55aedfa.js"
+  },
+  {
+    "revision": "d3e7bccd047089a7c5a7ee68532797f3",
+    "url": "/static/media/VIPAlarm.d3e7bccd.mp3"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0a17187927b57363cbdc4adb9104a0a.js"
+  "/precache-manifest.3ae3a1227abcfaceab39ac4df1920a61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
